85cc49548baac9e902c1f786b6da417150626fc1
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-10-30 16:51:30 -0700

    skills implemented

---
+++ src/components/lenguajes/Lenguajes.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./lenguajes.css"
+import VelocityScroll from './VelocityScroll';
+
+const Lenguajes = () => {
+    return (
+        <>
+            
+            <h3 className='skills__front'>Frontend</h3>
+            <div className='lenguajes'>
+                <section className="velocity_scroll">
+                    <VelocityScroll baseVelocity={-4}>
+                        <img src='../html.png' alt="" />
+                        <img src="../css.png" alt="" />
+                        <img src="../js.png" alt="" />
+                        <img src="../react.png" alt="" />
+                        <img src="../next.png" alt="" />
+                </VelocityScroll>
+                </section>
+            </div>
+            <h3>BackEnd</h3>
+            <div className='lenguajes'>
+                <section className="velocity_scroll">
+                    <VelocityScroll baseVelocity={4}>
+                        <img src="../node.png" alt="" />
+                        <img src="../postgres.png" alt="" />
+                        <img src="../mysql.png" alt="" />
+                        <img src="../mongodb.png" alt="" />
+                        <img src="../firebase.png" alt="" />
+                        <img src="../express.png" alt="" />
+                    </VelocityScroll>
+                </section>
+            </div>
+            <h3>More Technologies</h3>
+            <div className='lenguajes'>
+                <section className="velocity_scroll">
+                    <VelocityScroll baseVelocity={-4}>
+                        <img src="../git.png" alt="" />
+                        <img src="../github.png" alt="" />
+                        <img src="../swiper.png" alt="" />
+                        <img src="../semantic.png" alt="" />
+                        <img src="../particles.png" alt="" />
+                        <img src="../motion.png" alt="" />
+                        
+                    </VelocityScroll>
+                </section>
+            </div>
+            <h3></h3>
+        </>
+    );
+};
+
+export default Lenguajes;
--- src/components/ui/Skills.jsx
+++ src/components/ui/Skills.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Lenguajes from '../lenguajes/Lenguajes';
+import Typewriter from 'typewriter-effect';
 
 const Skills = () => {
   return (
     <section id='skills' className='skills'>
-     <div className='container'>
-      <h3 className='skills__title'> My skills and technologies nowadays </h3>
-     </div>   
+     <h3 className='skills__principal'> 
+                        <Typewriter 
+                            options={{
+                                strings: [  'My Skills', 
+                                            ],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        /> </h3> 
+     
+     <Lenguajes />
     </section>
   )
 }
