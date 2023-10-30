2dfb93d8ddd9592fd8b6857697f7f9efecac1d4d
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-10-30 12:09:50 -0700

    about implemented

---
+++ src/components/ui/About.jsx
@@ -0,0 +1,29 @@
+
+const About = () => {
+
+
+    return (
+      <section id="about" className="aboutt">
+        <div className="bgTitle container"> 
+          <p className="a-title"> About me..</p>
+        </div>
+        <section className="about container">
+            <figure className="about__figure">
+                <img className="about__img" src="../about.jpg" />
+            </figure>
+            <article className="about__article">
+                <p className="about__description"> Hello, my name is Guillermo Antonio Sandoval Espinoza, I was born in Mazatlán, México, 
+                    I am a passionate programmer always in the best disposition to colaborate in any task or challenge that happens. 
+                    Every day I feel the need to learn something new, nowadays I have knowledge and skills on avant-garde technologies
+                    in FullStack Develop field, which I would be happy to contribute along with my soft skills, to achieve corporate, team, 
+                    and personal objectives.    
+                </p>
+            </article>  
+        </section>
+      </section>
+      
+    )
+  }
+  
+  export default About
+  
