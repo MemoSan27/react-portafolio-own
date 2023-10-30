2f04900573210091f4d8d8dce63f617149a20179
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-10-30 11:39:04 -0700

    Project portafolio

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,38 @@
+import Typewriter from 'typewriter-effect';
+import NavBar from './Navbar';
+import Cubo from '../swiper/Cubo';
+
+const Header = () => {
+
+
+    return (
+        <header id='header' className='header'>
+            <NavBar />
+            <div className='header__box container'>
+                <div className='header__box-info container'>
+                        <img className='header__box-img' src='../profileScreenshot.jpg' alt='Img Profile' />
+                        <Typewriter
+                            options={{
+                                strings: [  'Frontend', 
+                                            'Backend',    
+                                            'Developer'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                        <div className='header__box-icons'>
+                            <a className='header__icon-link' href='https://www.linkedin.com/in/guillemo-sandoval/' target='_blank'>
+                                <i className='bx bxl-linkedin' ></i>    
+                            </a>
+                            <a className='header__icon-gith' href='https://github.com/MemoSan27' target='_blank'>
+                                <i className='bx bxl-github' ></i>  
+                            </a>
+                        </div>
+                </div>
+                <Cubo />
+             </div>
+        </header>
+      )
+    }
+
+export default Header
